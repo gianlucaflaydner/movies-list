b398a8beccf4fe8dc58ef17d1825ca57ec2e30b5
Gianluca Laydner <gianlucaflaydner@gmail.com>
2024-04-17 19:35:17 -0300

    chore: change resolver url to use env variable

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,8 +1,10 @@
+const urlToFetch = process.env.NEXT_PUBLIC_URL_LOCAL_MOVIES as string;
+
 const resolvers = {
   Query: {
     getMovies: async () => {
       try {
-        const response = await fetch("http://localhost:3000/api/movies", {
+        const response = await fetch(urlToFetch, {
           method: "GET",
         });
         const data = await response.json();
